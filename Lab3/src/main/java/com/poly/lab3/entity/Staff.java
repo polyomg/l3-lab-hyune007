01f61d7564b01bdc43e13d019ad6e9b150956de9
HyuMorreti <huyhuyhuy201107@gmail.com>
2025-10-02 20:40:18 +0700

    TS00689 - Nguyen Truong Huy - Lab3

---
+++ Lab3/src/main/java/com/poly/lab3/entity/Staff.java
@@ -0,0 +1,28 @@
+package com.poly.lab3.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Staff {
+    private String id;
+    private String fullname;
+    @Default
+    private String photo ="photo.jpg";
+    @Default
+    private Boolean gender=true;
+    @Default
+    private Date birthdate = new Date();
+    @Default
+    private Double salary =1234.5678;
+    @Default
+    private Integer level = 0;
+}
